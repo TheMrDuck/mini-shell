ca61b3501355c4e1f87a36076301641471d7b429
Hamzah <hamzahtaimour@gmail.com>
2025-06-24 13:56:33 +0300

    tabs

--- expansion.c
+++ expansion.c
@@ -2,34 +2,34 @@
 
 int check_env_var(char *token)
 {
-    if (!token || !*token)
-        return (0);
-    if (token[0] == '\'')
-        return (0);
-    if (strchr(token, '$') == NULL)
-        return (0);
-    return (1);
+	if (!token || !*token)
+		return (0);
+	if (token[0] == '\'')
+		return (0);
+	if (strchr(token, '$') == NULL)
+		return (0);
+	return (1);
 }
 
 
 
 void    expand(t_tokens **tokens, char **envp)
 {
-    t_tokens    *current;
+	t_tokens    *current;
 
-    if (!tokens || !*tokens || !envp)
-        return;
-    current = *tokens;
-    while (current)
-    {
-        if (current->type == TOKEN_WORD)
-        {
-            if (check_env_var(current->token))
-            {
-                expand_env_var(current, envp);
-                continue;
-            }
-        }
-        current = current->next;
-    }
+	if (!tokens || !*tokens || !envp)
+		return;
+	current = *tokens;
+	while (current)
+	{
+		if (current->type == TOKEN_WORD)
+		{
+			if (check_env_var(current->token))
+			{
+				expand_env_var(current, envp);
+				continue;
+			}
+		}
+		current = current->next;
+	}
 }
